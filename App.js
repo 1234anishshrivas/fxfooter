81526d4d94047ce65649e0f30c19aacb67ec2e02
1234anishshrivas <143332860+1234anishshrivas@users.noreply.github.com>
2024-05-20 18:03:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer.js";
+function App() {
+    return (
+        <div>
+            <div
+                style={{
+                    minHeight: "400px",
+                    color: "green",
+                }}
+            >
+                <h1></h1>
+            </div>
+            <Footer />
+        </div>
+    );
+}
+ 
+export default App;
